44af94c8e24dea105cc55e4eee468059bb88e043
Guilherme Gules <guilhermegules@gmail.com>
2022-09-17 12:00:27 -0300

    feat: creating svelte sample

---
+++ web-components-with-web-frameworks/svelte-web-components-poc/src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  import '../../web-components/hello-world'
+  import '../../web-components/counter-wc'
+
+  let color = "red";
+  let increment = 1;
+
+  function incrementByOne() {
+    increment = 1;
+  }
+
+  function incrementByTwo() {
+    increment = 2;
+  }
+</script>
+
+<main>
+  <hello-world></hello-world>
+
+  <button on:click="{incrementByOne}">Increment by 1</button>
+  <button on:click="{incrementByTwo}">Increment by 2</button>
+
+  <select bind:value={color}>
+    <option value="red">Red</option>
+    <option value="green">Green</option>
+    <option value="blue">Blue</option>
+  </select>
+
+  <counter-wc color={color} increment={increment}></counter-wc>
+</main>
+
+
